ad1588d0736f6c36ed1665387bc6a8ac5e6524bb
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2025-04-20 23:09:30 +1200

    chore: format input

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -2,9 +2,47 @@ import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { Cog, Fuel, GaugeCircle, Paintbrush } from 'lucide-react'
 import type { CarWithImages } from '@/config/type'
+import { OdoUnit, Transmission, FuelType } from '@prisma/client'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+const formatNumber = (
+  num: number | null,
+  options?: Intl.NumberFormatOptions
+) => {
+  if (!num) return '0'
+
+  return new Intl.NumberFormat('en-GB', options).format(num)
+}
+
+const formatOdometer = (unit: OdoUnit) => {
+  return unit === OdoUnit.MILES ? 'mi' : 'km'
+}
+
+const formatTransmission = (transmission: Transmission) => {
+  return transmission === Transmission.AUTOMATIC ? 'Automatic' : 'Manual'
+}
+
+const formatFuelType = (fuelType: FuelType) => {
+  switch (fuelType) {
+    case FuelType.PETROL:
+      return 'Petrol'
+    case FuelType.DIESEL:
+      return 'Diesel'
+    case FuelType.ELECTRIC:
+      return 'Electric'
+    case FuelType.HYBRID:
+      return 'Hybrid'
+    default:
+      return 'Unknown'
+  }
+}
+
+const formatColour = (colour: string) => {
+  if (!colour || typeof colour !== 'string') return ''
+  return colour[0] + colour.slice(1, colour.length).toLowerCase()
 }
 
 export const getKeyClassifiedInfo = (car: CarWithImages) => {
@@ -12,22 +50,22 @@ export const getKeyClassifiedInfo = (car: CarWithImages) => {
     {
       id: 'odoReading',
       icon: <GaugeCircle className="w-4 h-4" />,
-      value: `${car.odoReading} ${car.odoUnit}`,
+      value: `${formatNumber(car.odoReading)} ${formatOdometer(car.odoUnit)}`,
     },
     {
       id: 'transmission',
       icon: <Cog className="w-4 h-4" />,
-      value: car?.transmission,
+      value: formatTransmission(car?.transmission),
     },
     {
       id: 'fuelType',
       icon: <Fuel className="w-4 h-4" />,
-      value: car?.fuelType,
+      value: formatFuelType(car?.fuelType),
     },
     {
       id: 'colour',
       icon: <Paintbrush className="w-4 h-4" />,
-      value: car?.colour,
+      value: formatColour(car?.colour),
     },
   ]
 }
